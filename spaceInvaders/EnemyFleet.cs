4d4c67622f72472612fc4453f97926a538a47cb3
Josh McDonough <mcdojl4@student.op.ac.nz>
2022-11-08 14:56:53 +1300

    Touch up on Enemy Fleet

--- spaceInvaders/EnemyFleet.cs
+++ spaceInvaders/EnemyFleet.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace spaceInvaders
 {
-    internal class EnemyFleet
+    public class EnemyFleet
     {
+        //Enemy vars
+        private const int enemy_VEL = 20;
+        private const int enemy_X = 375;
+        private const int enemy_Y = 500;
+        private const int enemy_WIDTH = 30;
+        private const int enemy_HEIGHT = 50;
+
+        private List<Enemy> enemies;
+
+        public EnemyFleet(List<Enemy> enemies)
+        {
+            this.enemies = enemies;
+
+            enemies = new List<Enemy>();
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                    enemies.Add(new Enemy(new Rectangle(i + 20, j + 20, enemy_WIDTH, enemy_HEIGHT),
+                    Color.Blue,
+                    boundries,
+                    graphics,
+                    new Point(enemy_VEL, enemy_VEL)));
+
+                }
+            }
+        }
     }
 }
